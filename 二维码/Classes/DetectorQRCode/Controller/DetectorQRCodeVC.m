766a4653f18e82c3223f72a1d18f3fbcc4b3c99c
chenxu89 <chenxu@chenxudeimac.local>
2016-09-05 13:02:46 +0800

    识别图片二维码

--- 二维码/Classes/DetectorQRCode/Controller/DetectorQRCodeVC.m
+++ 二维码/Classes/DetectorQRCode/Controller/DetectorQRCodeVC.m
@@ -7,7 +7,30 @@
 //
 
 #import "DetectorQRCodeVC.h"
+#import <CoreImage/CoreImage.h>
+
+@interface DetectorQRCodeVC ()
+@property (weak, nonatomic) IBOutlet UIImageView *souceImageView;
+
+@end
 
 @implementation DetectorQRCodeVC
+- (IBAction)detectQRCode {
+    // 1.获取需要识别的图片
+    UIImage *image = self.souceImageView.image;
+//    CIImage *imageCI = image.CIImage;//这样得到的imageCI是nil
+    CIImage *imageCI = [[CIImage alloc] initWithCGImage:image.CGImage];
+    // 2.创建一个二维码探测器
+    CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeQRCode context:nil options:@{CIDetectorAccuracy : CIDetectorAccuracyHigh}];
+    // 3.直接探测二维码特征
+    NSArray *features = [detector featuresInImage:imageCI];
+    for (CIFeature *feature in features){
+//        NSLog(@"%@", feature);
+        CIQRCodeFeature *qrFeature = (CIQRCodeFeature *)feature;
+        NSLog(@"%@", qrFeature.messageString);
+        NSLog(@"%@", NSStringFromCGRect(qrFeature.bounds));
+    }
+    
+}
 
 @end
