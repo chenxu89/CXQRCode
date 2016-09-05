fc664843d2f2060b712c426c9f5a7a792777a269
chenxu89 <chenxu@chenxudeimac.local>
2016-09-05 09:56:29 +0800

    Initial Commit

---
+++ 二维码/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  二维码
+//
+//  Created by chenxu on 16/9/5.
+//  Copyright © 2016年 chenxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 二维码/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  二维码
+//
+//  Created by chenxu on 16/9/5.
+//  Copyright © 2016年 chenxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
