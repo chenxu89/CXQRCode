7ec123aa3acd5d1e3e387d3fbb7d1e08ec9d1565
chenxu89 <chenxu@chenxudeiMac.local>
2016-09-05 16:48:14 +0800

    实现扫描动画
    实现扫描动画

--- 二维码/Classes/ScanQRCode/Controller/ScanQRCodeVC.m
+++ 二维码/Classes/ScanQRCode/Controller/ScanQRCodeVC.m
@@ -8,6 +8,39 @@
 
 #import "ScanQRCodeVC.h"
 
+@interface ScanQRCodeVC ()
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *toBottom;
+@property (weak, nonatomic) IBOutlet UIView *scanBackView;
+@property (weak, nonatomic) IBOutlet UIImageView *chongjiboImageView;
+
+@end
+
 @implementation ScanQRCodeVC
 
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    
+    [self startScanAnimation];
+}
+
+- (void)startScanAnimation{
+    self.toBottom.constant = self.scanBackView.frame.size.height;
+    [self.view layoutIfNeeded];
+    self.toBottom.constant = 0;
+
+    [UIView animateWithDuration:2.0 delay:0.0 options:UIViewAnimationOptionCurveLinear animations:^{
+        [UIView setAnimationRepeatCount:MAXFLOAT];
+        [self.view layoutIfNeeded];
+    } completion:^(BOOL finished) {
+        
+    }];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self removeScanAnimation];
+}
+- (void)removeScanAnimation{
+    [self.chongjiboImageView.layer removeAllAnimations];
+    self.chongjiboImageView.hidden = YES;
+}
 @end
